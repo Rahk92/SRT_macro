526d31bad46d51dbbc034890559a57690e2de731
Rahk92 <43774749+Rahk92@users.noreply.github.com>
2024-02-07 13:29:58 +0900

    Delete macro_down.bat

--- macro_down.bat
+++
@@ -1,4 +0,0 @@
-F:
-cd F:\Code\Python\srt_reservation-main
-python quickstart_telegram.py --user 1683735937 --psw kang21259 --dpt 수서 --arr 광주송정 --dt 20230927 --tm 19 --num 3 --quantity 1 --special True
-pause
